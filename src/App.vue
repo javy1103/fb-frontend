e9a0a90758cae381980e1ea962211b235c7d167a
jlc <javy1103@gmail.com>
2016-11-03 23:40:48 -0500

    First Bundle

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container" id="app">
+        <component :is="currentComponent"></component>
+        <div>
+            <h2>Essential Links</h2>
+            <ul>
+                <li><a v-on:click=" currentComponent = 'Login' ">Login</a></li>
+                <li><a v-on:click=" currentComponent = 'Registration' ">Register</a></li>
+                <li><a v-on:click="showCurrent">showCurrent</a></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+import Login from './components/Login.vue'
+import Registration from './components/Registration.vue'
+
+export default {
+
+    name: 'app',
+
+    components: {
+        Login,
+        Registration
+    },
+
+    data() {
+        return {
+            currentComponent: 'Login'
+        }
+    },
+
+    methods: {
+        showCurrent() {
+            console.log(this.currentComponent)
+        }
+    }
+}
+</script>
---
+++ src/components/Login.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="hello">
+        <img src="http://vuejs.org/images/logo.png">
+        <h1>{{ msg }}</h1>
+        <form class="form-signin">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Remember me
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'login',
+    data () {
+        return {
+            msg: 'Login to Your Vue.js App'
+        }
+    }
+}
+</script>
+
+<style scoped>
+#hello {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+h1, h2 {
+    font-weight: normal;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #42b983;
+}
+</style>
